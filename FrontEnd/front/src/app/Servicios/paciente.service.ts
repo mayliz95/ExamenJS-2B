c5e12f853b7b2388cc0e00d8ee06b2fd7bb86232
mayliz95 <mayra.rosero@epn.edu.ec>
2018-07-18 16:22:00 -0500

    Busqueda sensitiva

--- FrontEnd/front/src/app/Servicios/paciente.service.ts
+++ FrontEnd/front/src/app/Servicios/paciente.service.ts
@@ -14,14 +14,17 @@ export class PacienteService {
     headers.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin, Access-Control-Allow-Methods");
     return headers;
   }
-
   getPacientes() {
     let header = PacienteService.getCommonHeaders();
     return this.http.get("http://localhost:1337/paciente",{headers: header});
   }
-
   getPacientePorId(idPaciente) {
     let header = PacienteService.getCommonHeaders();
     return this.http.get("http://localhost:1337/paciente/" + idPaciente ,{headers: header});
+  }
+  buscarPaciente(parametroBusqueda) {
+    let header = PacienteService.getCommonHeaders();
+    return this.http.get('http://localhost:1337/paciente?where={"nombres":{"contains":"' + parametroBusqueda + '"}}'
+     ,{headers: header});
   }
 }
--- FrontEnd/front/src/app/home/home.component.ts
+++ FrontEnd/front/src/app/home/home.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {MedicamentoService} from "../Servicios/medicamento.service";
 import {Router} from "@angular/router";
+import {PacienteService} from "../Servicios/paciente.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [MedicamentoService]
+  providers: [MedicamentoService, PacienteService]
 })
 export class HomeComponent implements OnInit {
 
   medicamentos = [];
+  datoABuscar;
 
-  constructor(private medicamentosService: MedicamentoService){
+  constructor(private medicamentosService: MedicamentoService,
+              private pacienteService: PacienteService
+  ){
   }
-
   ngOnInit() {
     this.medicamentosService.getMedicamentos().subscribe(
       (result: any[]) => {
@@ -22,5 +25,17 @@ export class HomeComponent implements OnInit {
       }
     );
   }
-
+  cargarDatosbusqueda() {
+    this.medicamentosService.buscarMedicamento(this.datoABuscar).subscribe(
+      (result: any[]) => {
+        this.medicamentos = result;
+      }
+    );
+    /*this.pacienteService.buscarPaciente(this.datoABuscar).subscribe(
+      (result: any[]) => {
+        this.medicamentos = result;
+        console.log(this.medicamentos)
+      }
+    );*/
+  }
 }
